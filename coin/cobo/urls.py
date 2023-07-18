70ee4d3246fd14fc55db1a2f4f61152c1cf637de
Kyu Ri Park <kyuripark@Kyuui-MacBookAir.local>
2023-07-18 20:54:37 +0900

    merge error

--- coin/cobo/urls.py
+++ coin/cobo/urls.py
@@ -5,8 +5,8 @@ from .views import base
 app_name = 'cobo'
 
 urlpatterns = [
-    path('', views/base.board_list, name='board_list'),
-    path('post/create/', views/base.post_create, name='post_create'),
-    path('post_detail', views/base.post_detail, name='post_detail'),  # 새 게시물 작성 페이지 URL
+    path('', base.board_list, name='board_list'),
+    path('post/create/', base.post_create, name='post_create'),
+    path('post_detail', base.post_detail, name='post_detail'),  # 새 게시물 작성 페이지 URL
 ]
 
