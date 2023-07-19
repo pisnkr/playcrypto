1dff97bd90be95782faa79295999f11e8047d314
Kyu Ri Park <kyuripark@Kyuui-MacBookAir.local>
2023-07-19 11:03:34 +0900

    account added

--- coin/cobo/urls.py
+++ coin/cobo/urls.py
@@ -1,12 +1,12 @@
-
+# cobo/urls.py
 from django.urls import path
 from . import views
 
 app_name = 'cobo'
 
+# cobo/urls.py
 urlpatterns = [
     path('', views.board_list, name='board_list'),
-    path('post/create/', views.post_create, name='post_create'),
+    path('post_create', views.post_create, name='post_create'), 
     path('post_detail', views.post_detail, name='post_detail'),  # 새 게시물 작성 페이지 URL
 ]
-
