c5de41acca0354c93b7e27c8c3029eb9b6914d2c
Kyu Ri Park <kyuripark@Kyuui-MacBookAir.local>
2023-08-07 11:39:37 +0900

    chatrooms2

--- coin/chat/urls.py
+++ coin/chat/urls.py
@@ -1,13 +1,26 @@
 # chat/urls.py
+# from django.urls import path
+# from . import views
+
+# app_name = 'chat'
+
+# urlpatterns = [
+#     path("", views.index, name="i"),
+#     path("<str:room_name>/", views.room, name="room"),
+#     path('chat_rooms/', views.chat_rooms, name='chat_rooms'),
+#     path('create_chat_room/', views.create_chat_room, name='create_chat_room'),
+
+# ]
+
+# chat/urls.py
 from django.urls import path
 from . import views
 
 app_name = 'chat'
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:room_name>/", views.room, name="room"),
-    path('chat_rooms/', views.chat_rooms, name='chat_rooms'),
-    path('create_chat_room/', views.create_chat_room, name='create_chat_room'),
-
+    path('', views.index, name='index'),  # 처음 메인 화면 뷰
+    path('chat_rooms/', views.chat_rooms, name='chat_rooms'),  # 채팅방 목록 뷰
+    path('create_chat_room/', views.create_chat_room, name='create_chat_room'),  # 새 채팅방 생성 뷰
+    path('<str:room_name>/', views.room, name='room'),  # 채팅방 뷰
 ]
--- coin/chat/views.py
+++ coin/chat/views.py
@@ -2,23 +2,36 @@
 from django.shortcuts import render, redirect
 from .models import ChatRoom
 from .forms import ChatRoomForm
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='common:login')  # 로그인된 사용자만 접근 가능
+def create_chat_room(request):
+    if request.method == 'POST':
+        room_name = request.POST.get('room_name')
+        chat_room = ChatRoom.objects.create(name=room_name)
+        return redirect('chat:index')
+    return redirect('chat:index')
+
+@login_required(login_url='common:login')  # 로그인된 사용자만 접근 가능
 def index(request):
-    return render(request, "chat/index.html")
+    chat_rooms = ChatRoom.objects.all()
+    return render(request, 'chat/index.html', {'chat_rooms': chat_rooms})
+
+# def index(request):
+#     chat_rooms = ChatRoom.objects.all()
+#     return render(request, 'chat/index.html', {'chat_rooms': chat_rooms})
 
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
 
-def chat_rooms(request):
-    rooms = Room.objects.all()  # 모든 채팅방 정보 가져오기
-    return render(request, 'chat/chat_rooms.html', {'rooms': rooms})
 
-def create_chat_room(request):
-    if request.method == 'POST':
-        form = ChatRoomForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('chat:chat_rooms')  # 채팅방 목록 페이지로 이동
-    else:
-        form = ChatRoomForm()
-    return render(request, 'chat/create_chat_room.html', {'form': form})
+def chat_rooms(request):
+    chat_rooms = ChatRoom.objects.all()
+    return render(request, 'chat/chat_rooms.html', {'chat_rooms': chat_rooms})
+
+# def create_chat_room(request):
+#     if request.method == 'POST':
+#         room_name = request.POST.get('room_name')
+#         chat_room = ChatRoom.objects.create(name=room_name)
+#         return redirect('chat:index')  # 생성 후 메인 페이지로 이동
+#     return redirect('chat:index')
